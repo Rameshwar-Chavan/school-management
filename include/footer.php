413a38c6d559b05516b49a521a2f183f256ab939
ForePlus Technosolutions <chavanrm234@gmail.com>
2019-08-07 18:51:30 +0530

    added css

---
+++ include/footer.php
@@ -0,0 +1,6 @@
+
+  <footer>School Management @copyright 2019 school;<?php date('Y')?></footer>
+  
+    <script src="/js/bootstrap.main.js"></script>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/bootstrap.main.css">
+    <title>School Management</title>
+</head>
+<body>
+    <nav class="nav-light">
+        <li>
+            <a href="index.php">Home</a>
+            <a class="nav" href="admission.php">Admission</a>
+            <a href="about-us.php">About Us</a>
+            <a href="contact-us.php">Contact US</a>
+        </li>
+    </nav>
